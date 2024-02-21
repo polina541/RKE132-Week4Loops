d09e33c7f7988e2d5fa7ac3931d2fec7cd63ed0d
Polina Smm. <polina.somova@tktk.ee>
2024-02-21 16:17:47 +0200

    add Week4Loops code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿Random rnd = new Random();
+
+
+
+int randomSum = 0;
+for (int  i = 0; i < 5; i++) 
+{
+
+    int myRandomNum = rnd.Next(0, 15);  //0 - 14 
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"Your random number is: {myRandomNum}");
+
+}
+Console.WriteLine($"random sum total: {randomSum}");
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,24 @@
+﻿Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true; //bool - true of false
+
+while (loopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    //Console.WriteLine($"cpu has generated {cpuRandom}");
+    Console.WriteLine("Make a guess. Enter a number 1-3");
+    int userGuess = Int32.Parse(Console.ReadLine()); 
+
+    if (userGuess == cpuRandom)
+    {
+        Console.WriteLine("Congratulations, your got it!");
+        loopActive = false;
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Oopsie, try again");
+    }
+}
+Console.WriteLine("The end of the game");
